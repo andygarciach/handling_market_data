9f01eaa2e69bfdcaf1b0aa941048a55d9b132644
Andy Garcia <andy.garcia.chavez@gmail.com>
2024-08-03 22:47:30 -0500

    fix: spark session builder final
    Fixed spark session builder to enable data processing

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -2,14 +2,24 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from utils import Utils
 from utils.spark_utils import Spark_utils
-
 import logging
 
-def main():
-    ##spark = spark_utils.get_spark_session("DEV", "Meli_Datapipeline")
 
-    spark = Spark_utils("DEV", "Meli_Pipeline").set_spark_session(self)
-    spark.getOrCreate()
+def main():
+    spark = None
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
     
+    spark_util = Spark_utils("DEV", "Meli_Pipeline")
+    spark_util.set_spark_session()
+    spark = spark_util.get_spark_session()
+    
+    logger.info("Reading Data Sources from Local Storage...")
+    df_prints = spark.read.json("data_sources/prints.json")
+    df_taps = spark.read.json("data_sources/taps.json")
+    df_pays = spark.read.option("header","True").csv("data_sources/pays.csv")
+    logger.info("3 New Datasets were created")
+
+
 if __name__ == '__main__':
     main()
--- utils/spark_utils.py
+++ utils/spark_utils.py
@@ -5,40 +5,27 @@ class Spark_utils():
     def __init__(self, env: str, appName: str):
         self.env = env
         self.appName = appName
+        self.spark = None
 
-    @staticmethod
-    def set_spark_session(self) -> SparkSession:
-        spark = SparkSession.getActiveSession()
-        if spark is not None:     
-            return spark
-        spark = self.__create_spark_session(spark)    
-        return spark
     
-    def create_spark_session(self, spark: SparkSession) -> SparkSession.builder:
+    def set_spark_session(self) -> SparkSession:
+        self.spark = SparkSession.getActiveSession()
+        if self.spark is not None:     
+            return spark
+        self.spark = self.__create_spark_session()    
+        
+    
+    def __create_spark_session(self) -> SparkSession.builder:
         master : str = 'local'
-        spark_session_builder = spark
+        spark_session_builder = None
         print("Environment: ", self.env)
         try: 
             if self.env == "DEV" :
-                print("Accedi a la condicion")
                 spark_session_builder = SparkSession.builder.master(master).appName(self.appName)
         except Exception as e:
-            pass    
-        return spark_session_builder
+            print("Error al crear SparkSession: ", str(e))
+        return spark_session_builder.getOrCreate()
 
-def get_spark_session(env, app_name):
-    if env == 'DEV':
-        spark = SparkSession. \
-            builder. \
-            master('local'). \
-            appName(app_name). \
-            getOrCreate()
-        return spark
-    elif env == 'PROD':
-        spark = SparkSession. \
-            builder. \
-            master('yarn'). \
-            appName(app_name). \
-            getOrCreate()
-        return spark
-    return
+    def get_spark_session(self):
+        return self.spark
+
